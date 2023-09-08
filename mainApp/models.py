83713063cf880dcfac7c76885bc9f46094f862d9
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-09-08 14:18:48 +0530

    initialcommit

--- mainApp/models.py
+++ mainApp/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Employee(models.Model):
     id =models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
-    email=models.EmailField(max_length=13)
+    email=models.EmailField(max_length=30)
     dsg=models.CharField(max_length=20)
+    phone=models.CharField(max_length=15,default="")   
     salary=models.IntegerField()
     city=models.CharField(max_length=20,default="",null=True, blank=True)
     state=models.CharField(max_length=20,default="",null=True, blank=True)
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Employee
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+def homepage(Request):
+    data = Employee.objects.all().order_by("-id")
+    paginator = Paginator(data, 3)  # Show 25 contacts per page.
+    page_number = Request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(Request, "index.html", {"data": page_obj})
+
