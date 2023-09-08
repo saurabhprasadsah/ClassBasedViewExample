16a9d3ed2d77ce2373f695bd7133ac3a39a5d1f5
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-09-08 16:31:15 +0530

    initialcommit

--- mainApp/views.py
+++ mainApp/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 
 def homepage(Request):
     data = Employee.objects.all().order_by("-id")
-    paginator = Paginator(data, 3)  # Show 25 contacts per page.
+    paginator = Paginator(data, 1)  # Show 25 contacts per page.
     page_number = Request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     return render(Request, "index.html", {"data": page_obj})
