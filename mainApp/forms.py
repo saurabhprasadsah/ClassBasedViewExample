3982029b4ece7fdb537b55b885cf86ac244caa48
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-09-08 21:57:19 +0530

    forms

--- mainApp/forms.py
+++ mainApp/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from .models import Employee
 
-
 class EmplyeeForm(forms.ModelForm):
     class Meta:
         model =Employee
         fields= ['name','email','phone','dsg','salary','city','state']
+        widgets= {
+            'name':forms.TextInput(attrs={'class':'forms.control','placeholder':'Enter name'}),
+            'email':forms.TextInput(attrs={'class':'forms.control','placeholder':'Enter email id'}),
+            'phone':forms.TextInput(attrs={'class':'forms.control','placeholder':'Enter phone Number'}),
+            'dsg':forms.TextInput(attrs={'class':'forms.control','placeholder':'Enter Designation'}),
+            'salary':forms.TextInput(attrs={'class':'forms.control','placeholder':'Enter salary'}),
+            'city':forms.TextInput(attrs={'class':'forms.control','placeholder':'Enter city Name'}),
+            'state':forms.TextInput(attrs={'class':'forms.control','placeholder':'Enter state Name'})
+        } 
